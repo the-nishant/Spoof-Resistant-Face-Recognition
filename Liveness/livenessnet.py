a9c939d76161351c7d86a7997a6cf20bd436db13
Nishant Abhangi <nishant.abhangi@gmail.com>
2020-06-26 15:24:18 +0530

    Cleaned unnecessary code of  livenessnet.py

--- Liveness/livenessnet.py
+++ Liveness/livenessnet.py
@@ -1,12 +1,6 @@
 # import the necessary packages
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Flatten, Dropout, Dense
 from tensorflow.keras import backend as K
 
 class LivenessNet:
@@ -24,47 +18,38 @@ class LivenessNet:
 			inputShape = (depth, height, width)
 			chanDim = 1
 
-		# first CONV => RELU => CONV => RELU => POOL layer set
-		model.add(Conv2D(16, (3, 3), padding="same",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
+		# first CONV => CONV => POOL block
+		model.add(Conv2D(16, 3, padding="same", activation="relu", input_shape=inputShape))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(16, (3, 3), padding="same"))
-		model.add(Activation("relu"))
+		model.add(Conv2D(16, 3, padding="same", activation="relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(MaxPooling2D(2))
 		model.add(Dropout(0.25))
 
-		# second CONV => RELU => CONV => RELU => POOL layer set
-		model.add(Conv2D(32, (3, 3), padding="same"))
-		model.add(Activation("relu"))
+		# second CONV => CONV => POOL block
+		model.add(Conv2D(32, 3, padding="same", activation="relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(32, (3, 3), padding="same"))
-		model.add(Activation("relu"))
+		model.add(Conv2D(32, 3, padding="same", activation="relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(MaxPooling2D(2))
 		model.add(Dropout(0.25))
 
-		# second CONV => RELU => CONV => RELU => POOL layer set
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
+		# third CONV => CONV => POOL block
+		model.add(Conv2D(64, 3, padding="same", activation="relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
+		model.add(Conv2D(64, 3, padding="same", activation="relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(MaxPooling2D(2))
 		model.add(Dropout(0.25))
 
-		# first (and only) set of FC => RELU layers
+		# first (and only) fully connected(Dense) layer
 		model.add(Flatten())
-		model.add(Dense(64))
-		model.add(Activation("relu"))
+		model.add(Dense(64, activation="relu"))
 		model.add(BatchNormalization())
 		model.add(Dropout(0.5))
 
 		# softmax classifier
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+		model.add(Dense(classes, activation="softmax"))
 
 		# return the constructed network architecture
 		return model
