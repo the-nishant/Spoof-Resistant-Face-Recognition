5ca47c371497fa308a3c5549f20bff3f6d2f47bd
Nishant Abhangi <nishant.abhangi@gmail.com>
2020-06-26 15:50:14 +0530

    Cleaned and updated encode_faces.py

--- encode_faces.py
+++ encode_faces.py
@@ -1,10 +1,11 @@
 # USAGE
-# python encode_faces.py --dataset dataset --encodings output/encodings.pickle 
+# python encode_2.py --dataset dataset_recognizer --dataset2 dataset2_recognizer --encodings output/encodings.pickle --detector face_detector
 
 # import the necessary packages
 from imutils import paths
+import imutils
+import numpy as np
 import face_recognition
-import dlib
 import argparse
 import pickle
 import cv2
@@ -12,50 +13,103 @@ import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", required=True,
-    help="path to input directory of faces + images")
-ap.add_argument("-e", "--encodings", required=True,
-    help="path to serialized db of facial encodings")
-ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-    help="face detection model to use: either `hog` or `cnn`")
+ap.add_argument("-i", "--dataset", help="path to input directory of unprocessed face images")
+ap.add_argument("-j", "--dataset2", help="path to input directory of faces")
+ap.add_argument("-e", "--encodings", required=True, help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detector", type=str, default="cnn", help="path to OpenCV's deep learning face detector")
+ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
-
-# grab the paths to the input images in our dataset
-print("[INFO] quantifying faces...")
-imagePaths = list(paths.list_images(args["dataset"]))
 
 # initialize the list of known encodings and known names
 knownEncodings = []
 knownNames = []
 
-# loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
-    # extract the person name from the image path
-    print("[INFO] processing image {}/{}".format(i + 1,
-        len(imagePaths)))
-    name = imagePath.split(os.path.sep)[-2]
-    if name in ["nishant2", "nishant3"]:
-    	continue
+if args["dataset"] is not None:
+    # load our serialized face detector from disk
+    print("[INFO] loading face detector...")
+    protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+    modelPath = os.path.sep.join([args["detector"],
+        "res10_300x300_ssd_iter_140000.caffemodel"])
+    net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-    # load the input image and convert it from RGB (OpenCV ordering)
-    # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # grab the paths to the input images in our dataset
+    print("[INFO] quantifying faces for dataset...")
+    imagePaths = list(paths.list_images(args["dataset"]))
 
-    (h,w) = image.shape[:2]
-    box = [(0,w,h,0)]
+    # loop over the image paths
+    for (i, imagePath) in enumerate(imagePaths):
+        # extract the person name from the image path
+        print("[INFO] processing image {}/{}".format(i + 1,
+            len(imagePaths)))
+        name = imagePath.split(os.path.sep)[-2]
 
-    
-    '''# detect the (x, y)-coordinates of the bounding boxes
-    # corresponding to each face in the input image
-    boxes = face_recognition.face_locations(rgb,
-        model=args["detection_method"])'''
+        # load the input image and convert it from RGB (OpenCV ordering)
+        # to dlib ordering (RGB)
+        image = cv2.imread(imagePath)
+        image = imutils.resize(image, width=600)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # compute the facial embedding for the face
-    encoding = face_recognition.face_encodings(rgb, box)
+         # grab the frame dimensions and convert it to a blob
+        (h, w) = image.shape[:2]
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 1.0, (300, 300),
+            (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-    knownEncodings.append(encoding)
-    knownNames.append(name)
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
+
+        # ensure at least one face was found
+        if len(detections) > 0:
+            # we're making the assumption that each image has only ONE
+            # face, so find the bounding box with the largest probability
+            i = np.argmax(detections[0, 0, :, 2])
+            confidence = detections[0, 0, i, 2]
+
+            # ensure that the detection with the largest probability also
+            # means our minimum probability test (thus helping filter out
+            # weak detections)
+            if confidence > args["confidence"]:
+                # compute the (x, y)-coordinates of the bounding box for
+                # the face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                box = [(startY, endX, endY, startX)]
+
+                # compute the facial embedding for the face
+                encoding = face_recognition.face_encodings(rgb, box)
+
+                # add each encoding + name to our set of known names and
+                # encodings
+                knownEncodings.append(encoding)
+                knownNames.append(name)
+
+if args["dataset2"] is not None:
+    # grab the paths to the input images in our dataset
+    print("[INFO] quantifying faces for dataset2...")
+    imagePaths = list(paths.list_images(args["dataset2"]))
+
+    # loop over the image paths
+    for (i, imagePath) in enumerate(imagePaths):
+        # extract the person name from the image path
+        print("[INFO] processing image {}/{}".format(i + 1,
+            len(imagePaths)))
+        name = imagePath.split(os.path.sep)[-2]
+
+        # load the input image and convert it from RGB (OpenCV ordering)
+        # to dlib ordering (RGB)
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # grab the frame dimensions and compute the bounding box
+        (h,w) = image.shape[:2]
+        box = [(0,w,h,0)]
+
+        # add each encoding + name to our set of known names and
+        # encodings
+        knownEncodings.append(encoding)
+        knownNames.append(name)
 
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
